385dbf0cf807a9fc8c57d46fa68ec0ac5c452205
Chandan <chandan.1139@zenmonk.tech>
2024-09-11 17:30:36 +0530

    halson

--- src/features/product/get-product/get-product.service.ts
+++ src/features/product/get-product/get-product.service.ts
@@ -1,33 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from 'src/infrastructure/repositories/user/user.repository';
 import { GetProduct } from './get-product.interface';
+import halson from 'halson';
 import { ProductRepository } from 'src/infrastructure/repositories/product/product.repository';
 
 @Injectable()
 export class GetProductHandler {
   constructor(private readonly productRepository: ProductRepository) {}
 
-  public async handle(payload: GetProduct) {
+  public async handle(payload: GetProduct): Promise<any> {
     try {
       const product = await this.productRepository.getProductByUUID(
         payload.uuid,
       );
-      const response = product;
-      response['_links'] = {
-        self: {
-          href: `/get-product?uuid=${product.uuid}`,
-        },
-        update_product: {
-          operation_id: 'update-product',
+      const response = halson(product)
+        .addLink('self', `/get-product?uuid=${product.uuid}`)
+        .addLink('update_product', {
           href: `/update-product?uuid=${product.uuid}`,
-          method: 'PUT',
-        },
-        delete_product: {
-          operation_id: 'delete-product',
-          href: `/delete-product?uuid=${product.uuid}`,
-          method: 'DELETE',
-        },
-      };
+          title: 'Dsasdas',
+        } as any); //added type any as 
+      // response['_links'] = {
+      //   self: {
+      //     href: `/get-product?uuid=${product.uuid}`,
+      //   },
+      //   update_product: {
+      //     operation_id: 'update-product',
+      //     href: `/update-product?uuid=${product.uuid}`,
+      //     method: 'PUT',
+      //   },
+      //   delete_product: {
+      //     operation_id: 'delete-product',
+      //     href: `/delete-product?uuid=${product.uuid}`,
+      //     method: 'DELETE',
+      //   },
+      // };
 
       response['_embedded'] = {
         user: {
